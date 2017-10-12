d0cbaade2241f3bbdbd31a58ea05e6088bf43c48
dksun <dk.sun@9street.org>
2017-10-12 19:27:19 +0800

    init

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ cd $curdir
 #go install -v -ldflags "-w -s" server/worker/main.go
 
 echo '###### building Mac App ... ...'
-GOPATH=$curdir/vendor:$curdir/programs CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $curdir/programs/bin/spider_client_mac $curdir/programs/src/server/worker/main.go
+GOPATH=$curdir/vendor:$curdir/programs CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $curdir/programs/bin/proxypool_mac $curdir/programs/src/main.go
 
 #echo '###### building Linux App ... ...'
 #CGO_ENABLED=0
@@ -18,7 +18,7 @@ GOPATH=$curdir/vendor:$curdir/programs CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go
 #go build -o linux_goworker  server/worker/main.go
 
 echo '###### building Linux App ... ...'
-GOPATH=$curdir/vendor:$curdir/programs CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $curdir/programs/bin/spider_client_linux $curdir/programs/src/server/worker/main.go
+GOPATH=$curdir/vendor:$curdir/programs CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $curdir/programs/bin/proxypool_linux $curdir/programs/src/main.go
 
 #
 #echo '###### building Windows App ... ...'
@@ -28,4 +28,4 @@ GOPATH=$curdir/vendor:$curdir/programs CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go 
 #go build -o goworker.exe  server/worker/main.go
 
 echo '###### building Windows App ... ...'
-GOPATH=$curdir/vendor:$curdir/programs CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $curdir/programs/bin/spider_client_linux $curdir/programs/src/server/worker/main.go
+GOPATH=$curdir/vendor:$curdir/programs CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $curdir/programs/bin/proxypool_linux $curdir/programs/src/main.go
